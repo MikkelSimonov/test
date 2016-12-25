b4148b1dc74b19a84a0f1dce9c7b4f0bc662d96d
Mihail Zarubin <mikkel27@mail.ru>
2016-12-25 15:51:01 +0200

    last commit

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+header ("Content-Type: text/html; charset=utf-8");
+echo "<!DOCTYPE html>
+<html>
 <head>
-	<meta charset="UTF-8">
-	<title>Document</title>
+<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">
+<title>Open Server</title>
 </head>
-<body>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga id dolor delectus voluptatem ad quis est harum ut, eos, accusantium iste doloribus esse. Deserunt vitae sit, velit minima optio numquam perspiciatis pariatur ut ratione blanditiis enim quibusdam, dicta eum unde quasi dolorum nostrum sed quo repudiandae eligendi ad ipsam sequi et modi. Mollitia labore, provident, repellat sunt commodi debitis tempore rem necessitatibus? Numquam itaque deleniti cumque accusamus tempore nesciunt rerum ut expedita, impedit culpa earum, doloremque a ex illum aliquam corrupti iusto adipisci. Ad blanditiis culpa inventore, quo non molestiae autem hic voluptate a alias nulla modi minus, illum cumque.
+<body style=\"background: url(fon.png) top left repeat-x\">
+<center>
+<br><br><br><div style=\"width: 600px;\"><span style=\"font-size: 32px; color: green; font-family: Arial, Verdana; text-shadow: 0 1px 0 #fff\">Добро пожаловать в Open Server!</span>
+<br><br><br><span style=\"font-size: 32px; color: #333; font-family: Verdana, Arial;\">Он работает ;-)</span>
+<br><img src=\"st.png\" style=\"margin: 40px 0\"><br><a href=\"http://open-server.ru/docs/\" style=\"font-size: 24px; color: #048acd; font-family: Arial;\">Руководство пользователя</a></span><br><br><br></div>
+</center>
 </body>
-</html>
+</html>";
+?>
